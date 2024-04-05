e3fcb657e4f422e8edfe584273ddcecd7971b447
Marcelo Augusto <marcelo.a32@gmail.com>
2024-04-04 23:30:16 -0300

    Java para web
    Listagem 2.6, pagina 85, com getters e setters

---
+++ teste/src/main/java/br/com/javaparaweb/teste/web/UsuarioBean.java
@@ -0,0 +1,39 @@
+package br.com.javaparaweb.teste.web;
+
+/**
+ * Pagina 85, Listagem 2.6 
+ *
+ */
+public class UsuarioBean {
+
+	private String ome;
+	private String email;
+	private String senha;
+	private String confirmaSenha;
+	public String getOme() {
+		return ome;
+	}
+	public void setOme(String ome) {
+		this.ome = ome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getConfirmaSenha() {
+		return confirmaSenha;
+	}
+	public void setConfirmaSenha(String confirmaSenha) {
+		this.confirmaSenha = confirmaSenha;
+	}
+
+
+}
